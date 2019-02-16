37ffbe0c9ff5d6494d14afd951440c2e9a9909ac
Santosh Purbey <santosh.purbey@logpoint.com>
2019-02-16 15:32:47 +0545

    add unique, validator in contact_no and choices in gender

--- apps/user_profile/models.py
+++ apps/user_profile/models.py
@@ -1,11 +1,31 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 
+def validate_mobile_no_is_numeric(mobile_no):
+    if not mobile_no.isnumeric():
+        raise ValidationError(
+            _('"%(num)s" is not valid mobile number'),
+            params={'num':mobile_no }
+            )
 class Profile(models.Model):
-    contact_no = models.CharField(max_length=15)
+    MALE = 'm'
+    FEMALE = 'f'
+
+    GENDER_CHOICE = (
+        (MALE, 'Male'),
+        (FEMALE, 'Female'),
+    )
+
+    contact_no = models.CharField(max_length=15, unique=True,
+                                verbose_name='Mobile No',
+                                help_text='98XXXXXXXX',
+                                validators=[validate_mobile_no_is_numeric,])
+
     address = models.CharField(max_length=50)
     nationality = models.CharField(max_length=20)
-    gender = models.CharField(max_length=6)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICE)
     dob = models.DateField()
     occupation = models.CharField(max_length=20)
 
