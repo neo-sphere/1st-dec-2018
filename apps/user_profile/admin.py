ceb7e6f2050cc3797099b4b59ec330b31937ad3f
Santosh Purbey <santosh.purbey@logpoint.com>
2019-02-17 15:13:44 +0545

    customize model admin

--- apps/user_profile/admin.py
+++ apps/user_profile/admin.py
@@ -8,6 +8,12 @@ from .models import Profile
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('contact_no', 'address', 'nationality', 'gender', 'dob', 'occupation', 'get_age')
+    list_display_links = ('contact_no', 'dob', 'occupation')
+    list_editable = ('gender', 'nationality')
+    list_per_page = 10
+    list_filter = ('gender', 'dob', 'nationality')
+    date_hierarchy = 'dob'
+    search_fields = ('contact_no', 'address')
 
     def get_age(self, instance):
         timedelta_obj = date.today() - instance.dob
