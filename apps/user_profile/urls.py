06bd5620ff355328467e1c83a9143d7c3a3fcad4
Santosh Purbey <santosh.purbey@logpoint.com>
2019-02-17 17:09:45 +0545

    render template based views

--- apps/user_profile/urls.py
+++ apps/user_profile/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import user_profile_home
+from .views import user_profile_home, profile
 
 urlpatterns = [
     path('home/', user_profile_home),
+    path('profile/', profile),
 ]
--- apps/user_profile/views.py
+++ apps/user_profile/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def user_profile_home(request):
-    return HttpResponse('Welcome to User Profile Home Page')
+    return HttpResponse('<h1>Welcome to User Profile Home Page</h1>')
+
+def profile(request):
+    template_name = 'profile.html'
+    return render(request, template_name)
