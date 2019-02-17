3e4235259ca7bdb2cf01b7f9a51ef632c0d150d4
Santosh Purbey <santosh.purbey@logpoint.com>
2019-02-17 14:59:03 +0545

    new style register model in admin, get_age method in admin

--- apps/user_profile/admin.py
+++ apps/user_profile/admin.py
@@ -1,7 +1,20 @@
+from datetime import date
+
 from django.contrib import admin
 
 from .models import Profile
 
-admin.site.register(Profile)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('contact_no', 'address', 'nationality', 'gender', 'dob', 'occupation', 'get_age')
+
+    def get_age(self, instance):
+        timedelta_obj = date.today() - instance.dob
+        return int(timedelta_obj.days // 365.25)
+
+    get_age.short_description = 'Age'
+
+# admin.site.register(Profile, ProfileAdmin) # old style
 
 
